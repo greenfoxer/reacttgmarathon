ab21e56d32883f0089942bb3c97c5c6a8dc52e93
roman <greenfoxer@gmail.com>
2021-10-01 15:21:08 +0300

    test

--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -1,78 +1,49 @@
 import React, {useState} from "react";
 import { NotificationManager } from "react-notifications";
+import { useSelector } from "react-redux";
 import { Redirect } from "react-router";
+import { selectAuth, signIn, logOut, signUp } from "../../store/auth";
 
 import LoginForm from "../LoginForm";
 import Menu from "../Menu";
 import Modal from "../Modal";
 import NavBar from "../NavBar";
 
-const auth = async (url, data) => {
-
-    const request = {
-        method: "POST",
-        body: JSON.stringify({
-            email : data.email,
-            password : data.password,
-            returnSecureToken : true
-        })
-    }
-    
-    const response = await fetch(url, request).then( res => res.json());
-
-    if(response.hasOwnProperty('error'))
-    {
-        NotificationManager.error(response.error.message,"Error");
-        return false;
-    }
-    else
-    {
-        localStorage.setItem('idToken', response.idToken);
-        NotificationManager.success("Login success!");
-        return true;
-    }
-}
-
 const MenuHeader = (bgActive) => {
     const [isMenuOpened, setMenuOpened] = useState(undefined);
     const [isOpenModal, setIsOpenModal] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("idToken") !== null);
+    const authContext = useSelector(selectAuth)
     const onMenuStateChange = () => {
         setMenuOpened(prevState => !prevState);
     }
     const handleClickLogin = () => {
-        console.log(isLoggedIn);
-        if(isLoggedIn)
+        console.log(authContext.isLoggedIn);
+        if(authContext.isLoggedIn)
         {
             localStorage.removeItem('idToken');
-            window.location.reload();
+            window.location.replace('/');
         }
         else
             setIsOpenModal(prevState => !prevState);
     }
     const handleSubmitLoginForm =  async (data) => {
-        const key='AIzaSyBpwM-_LN_TarR1NbNedA5hbeae-nmGaI4'
         if(data.isSignin)
         {
-
-            const signInURL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key}`;
-            const result = await auth(signInURL, data);
-            if(result)
+            signIn(data);
+            if(authContext.isLoggedIn)
                 setIsOpenModal(prevState => false);
         }
         else
         {
-            const signUpURL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
-            const result = await auth(signUpURL, data);
-            if(result)
+            signUp(data);
+            if(authContext.isLoggedIn)
                 setIsOpenModal(prevState => !prevState);
         }
-        setIsLoggedIn( prev => localStorage.getItem("idToken") !== null);
     }
     return(
         <React.Fragment>
             <Menu onMenuStateChange={onMenuStateChange} isMenuShowed={isMenuOpened} />
-            <NavBar isLoggedIn={isLoggedIn}
+            <NavBar isLoggedIn={authContext.isLoggedIn}
                     onMenuStateChange={onMenuStateChange} 
                     bgActive={bgActive} 
                     isMenuShowed={isMenuOpened}
